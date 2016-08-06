9a1b6ad7bef9d22987cf2167eab9bbd3e37fad8e
sencse90 <sencse90@users.noreply.github.com>
2016-08-06 21:40:48 +0530

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,27 +4,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 
 	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 * Login controller
 	 */
+	
 	public function index()
 	{
+		$this->load->library('session');
 		$this->load->view('login');
 	}
 	public function login_process()
 	{
+		if ($this->input->is_ajax_request()){
+			#Clean the username and password
+			$username=$this->security->xss_clean($this->input->post('username'));
+			$password=$this->security->xss_clean($this->input->post('password'));
+			$this->load->model('user_model');
+			$log = $this->user_model->login($username,$password);
+			if($log['status']=='success')
+			{
+				$this->load->library('session');
+				$sessionData = array(
+				   'username'  	=> $log['username'],
+				   'name'	 	=> $log['name']
+				);
+				$this->session->set_userdata($sessionData);					
+			} else {
+				$this->output->set_content_type('application/json')->set_output(json_encode($log['status']));
+			}
 
-		echo 'gooo';
+		} else {
+			$this->output->set_content_type('application/json')->set_output(json_encode('Please check your Username and Password'));
+		}
 	}
 }
