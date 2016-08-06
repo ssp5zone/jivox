6819b7944917915f2686fdfd707519d36333d027
sencse90 <sencse90@users.noreply.github.com>
2016-08-07 03:05:24 +0530

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,16 +1,23 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
 	/**
 	 * Login controller
 	 */
-	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('session');
+		$this->load->helper('url');
+	}
 	public function index()
 	{
-		$this->load->library('session');
-		$this->load->view('login');
+		if ($this->session->userdata('username')){
+			$this->output->set_header('location: /jivox/products/product');
+		} else {
+			$this->load->view('login');
+		}
 	}
 	public function login_process()
 	{
@@ -27,13 +34,25 @@ class Welcome extends CI_Controller {
 				   'username'  	=> $log['username'],
 				   'name'	 	=> $log['name']
 				);
-				$this->session->set_userdata($sessionData);					
-			} else {
-				$this->output->set_content_type('application/json')->set_output(json_encode($log['status']));
+				$this->session->set_userdata($sessionData);				
 			}
+			
+			$this->output->set_content_type('application/json')->set_output(json_encode($log['status']));			
 
 		} else {
 			$this->output->set_content_type('application/json')->set_output(json_encode('Please check your Username and Password'));
 		}
+	}
+
+	/**
+	 * Process the Logout.
+	 * Log the user out by destroying the session and redirecting to
+	 * the login page.
+	 */
+	public function logout()
+	{
+		$this->session->sess_destroy();
+		$this->load->helper('url');
+		$this->output->set_header('location:'.base_url());
 	}
 }
