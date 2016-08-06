c8ae283a6abb51b224a1df281271d2abdaa0b9b9
sencse90 <sencse90@users.noreply.github.com>
2016-08-06 19:59:51 +0530

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+  	<link rel="stylesheet" type="text/css" href="<?php echo ASSET_PATH ?>/css/login.css" />
 	<head>
 		<title>SVS SHOPING</title>		
 	</head>
 	<body>
-		<img src="SVS" id="svs" />
+		<img  id="svs" />
 		<div class="container">
 				<img src="#" alt="SVS" id="logo"/>
-				<?//php echo form_open('login/login_process',array('id' => 'loginform')); ?>
-				<form>
+				<form action='welcome/login_process' type='POST'>				
 				  <div>
 					LAN ID and Password<br /><br />
 					<input type="text" id="username" name="username" placeholder="Username" required /><br/>
 					<input type="password" id="password" name="password" placeholder="Password" required />
 				  </div>
-				  <button type="submit" id="login_btn" class="" disabled>Login</button>
+				  <button type="submit" id="login_btn" class="">Login</button>
 				  <br /><p id="loginTxt"></p>				  
 				</form>
 		</div>		
-		<script>var r = ['app/login']</script>
 		<script data-main="<?php echo ASSET_PATH ?>/js/main" src="<?php echo ASSET_PATH ?>/js/require.js"></script>
 	</body>
 </html>
