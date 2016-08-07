d6cf644417b323281293ceda1ed6aa338536f01c
sencse90 <sencse90@users.noreply.github.com>
2016-08-07 07:10:21 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,11 @@ require.config({
 	shim : {
         "jquery.form" 		 : { "deps" :['jquery'] },
 		"notify" 			 : { "deps" :['jquery'] },
-		"jquery-ui" 		 : { "deps" :['jquery'] },
-		"idle-timer" 		 : { "deps" :['jquery'] },
-		"jquery-editable" 	 : { "deps" :['jquery','jquery-ui'] },
-		"datatables" 		 : { "deps" :['jquery'] },
-		"sumoselect" 		 : { "deps" :['jquery'] },
-		"Chart.HorizontalBar": { "deps" :['Chart.min'] }
+		"jquery-ui" 		 : { "deps" :['jquery'] }
     },
     paths: {
 		"app"				: "../app",
-        "jquery"    		: "jquery.min",
-		"datatables"		: "jquery.dataTables",
-		"sumoselect"		: "jquery.sumoselect.min"
+        "jquery"    		: "jquery.min"
     }
 })
 
